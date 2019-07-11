88a8c2940dc0255bf2d064e66f460a966f9bf3c9
Kyunghoon Moon <myrmidonxd@gmail.com>
2019-07-11 22:07:05 +0900

    Modify exercise: leetcode/Permutations
    - Add permuteM which is using list monad for permute
    - Remove unnecessary parentheses

--- exercise/leetcode/Permutation.hs
+++ exercise/leetcode/Permutation.hs
@@ -8,13 +8,18 @@ import Data.List
 permute :: (Eq a) => [a] -> Int -> [[a]]
 permute _ 0 = [[]]
 permute xs k =
-    let attachSelected = (\x -> map (x:) $ permute (delete x xs) (k-1))
+    let attachSelected = \x -> map (x:) $ permute (delete x xs) (k-1)
     in concat $ map attachSelected xs
+
+-- permute using bind function of list monad
+permuteM :: (Eq a) => [a] -> Int -> [[a]]
+permuteM _ 0 = return []
+permuteM xs k = xs >>= \x -> map (x:) $ permute (delete x xs) (k-1)
 
 solution :: (Eq a) => [a] -> [[a]]
 solution xs = 
     let n = length xs
-    in permute xs n
+    in permuteM xs n
 
 {-
     If the length of xs is n, then 'permute xs k' produces P(n, k) permutations on xs. (k-permutations of n)
