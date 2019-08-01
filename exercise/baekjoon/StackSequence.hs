c80c9de7ba993f24a0e064e20cb854f5773f6495
KyungHoon Moon <kyunghoon@bagelcode.com>
2019-08-01 20:26:27 +0900

    Add exercise: baekjoon/StackSequence

--- exercise/baekjoon/StackSequence.hs
+++ exercise/baekjoon/StackSequence.hs
@@ -1,4 +1,11 @@
+{-
+    [Baekjoon Online Judge] 1874. Stack Sequence
+    https://www.acmicpc.net/problem/1874
+-}
+
 import Data.Maybe
+import Data.List
+import Data.Char
 
 type Stack = [Int]
 
@@ -23,40 +30,28 @@ pushNumber s = s
     , stack = push (head $ numbers s) (stack s)
     }
 
-popNumber :: SolveState -> (Int, SolveState)
+popNumber :: SolveState ->  SolveState
 popNumber s = 
-    let (x, newStack) = pop $ stack s
-    in (x, s {stack = newStack})
+    let (_, newStack) = pop $ stack s
+    in s {stack = newStack, inputSeq = tail $ inputSeq s}
 
 isPop :: SolveState -> Bool
-isPop (SolveState _ x:restSeq t:st) = x == t
-isPop _                             = False
+isPop (SolveState _ (x:restSeq) (t:st)) = x == t
+isPop _                                 = False
 
 isPush :: SolveState -> Bool
-isPush (SolveState n:nextNum x:restSeq t:st) = n <= x && t < x
-isPush (SolveState n:nextNum x:restSeq [])   = n <= x
-isPush _                                     = False
+isPush (SolveState (n:nextNum) (x:restSeq) (t:st)) = n <= x && t < x
+isPush (SolveState (n:nextNum) (x:restSeq) [])     = n <= x
+isPush _                                           = False
 
 isDone :: SolveState -> Bool
 isDone (SolveState [] [] []) = True
 isDone _                     = False
 
-{-
-isValid :: SolveState -> Bool
-isValid (SolveState _ [] _)                   = True
-isValid (SolveState [] _ [])                  = False
-isValid (SolveState [] seq st)                = seq == st
-isValid (SolveState n:nextNum x:restSeq [])   = n <= x
-isValid (SolveState n:nextNum x:restSeq t:st)
-    | x == t = True
-    | x > t && n <= x = True
-    | otherwise = False
-
-
 solution :: Int -> [Int] -> [String]
 solution n targetSeq = case solve initialSolveState [] of
-    Nothing        = ["NO"]
-    Just resultSeq = resultSeq
+    Nothing        -> ["NO"]
+    Just resultSeq -> reverse resultSeq
   where
     initialSolveState = SolveState 
         { numbers  = [1 .. n]
@@ -64,8 +59,16 @@ solution n targetSeq = case solve initialSolveState [] of
         , stack    = []
         }
     solve :: SolveState -> [String] -> Maybe [String]
-    solve (SolveState {inputSeq = []}) result       = Just result
-    solve (SolveState {numbers = [], stack = []}) _ = Nothing
-    solve (SolveState {inputSeq = (i:is)}) _        = Nothing -- WIP
-    solve _ _ = Nothing -- Temporary base case
--}
+    solve s resultSeq
+        | isDone s  = Just resultSeq
+        | isPush s  = solve (pushNumber s) ("+":resultSeq)
+        | isPop  s  = solve (popNumber s) ("-":resultSeq)
+        | otherwise = Nothing
+
+main = interact $ unlines 
+                . uncurry solution
+                . fromMaybe (0, [])
+                . uncons
+                . map read
+                . lines
+ 
